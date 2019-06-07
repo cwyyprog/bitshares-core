f47c115cd21e62829e242447398b20b40a32cddf
Qixing <cwyyprog@163.com>
2019-06-07 22:47:35 +0800

    Update appveyor.bash

--- appveyor.bash
+++ appveyor.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 export MSYSTEM=MINGW64
-wget http://repo.msys2.org/mingw/x86_64/mingw-w64-x86_64-boost-1.69.0-2-any.pkg.tar.xz
-pacman --noconfirm -U mingw-w64-x86_64-boost-1.69.0-2-any.pkg.tar.xz
+wget http://repo.msys2.org/mingw/x86_64/mingw-w64-x86_64-boost-1.67.0-2-any.pkg.tar.xz
+pacman --noconfirm -U mingw-w64-x86_64-boost-1.67.0-2-any.pkg.tar.xz
 #pacman --noconfirm -S  mingw-w64-x86_64-boost; #mingw-w64-x86_64-openssl
 mkdir /msys64
 mount C:/msys64 /msys64;
